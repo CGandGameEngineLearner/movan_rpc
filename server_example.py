7aa8c6721f9d3b07cc275cb45e9d5dbf17fa72f4
CGandGameEngineLearner <lifesize1@qq.com>
2025-03-27 18:00:48 +0800

    更新示例

--- server_example.py
+++ server_example.py
@@ -6,18 +6,17 @@ import time
 server = RPCServer('127.0.0.1', 9999)
 
 
-
-# 使用装饰器注册同步方法
-@server.method
-def server_add(a:int, b:int):
-    return a + b
-
-# 使用装饰器注册异步方法
+# 使用装饰器注册异步方法 (推荐使用) 
 @server.method
 async def server_add_async(a:int, b:int):
     await asyncio.sleep(1)  
     return a + b
 
+
+# 使用装饰器注册同步方法 服务器线程会阻塞在这里 直到计算得到结果才返回 (不推荐使用)
+@server.method
+def server_add(a:int, b:int):
+    return a + b
 
 
 server.run()
